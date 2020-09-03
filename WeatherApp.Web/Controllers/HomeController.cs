8de7845f7d742332e96555cb1004d39cb1b42ffd
FahadPervez <pervez.fahad@googlemail.com>
2020-09-03 16:47:19 +0100

    added controller

---
+++ WeatherApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿namespace WeatherApp.Web.Controllers
+{
+    #region using
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using WeatherApp.ModelLayer;
+    using WeatherApp.ServiceLayer.WeatherServices;
+
+    #endregion
+
+    /// <summary>
+    /// The home controller representing weather app
+    /// </summary>
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new WeatherDataViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult GetWeatherReport(WeatherDataViewModel weatherDataViewModel)
+        {
+            WeatherDataService weatherDataService = new WeatherDataService(new ExceptionService());
+            var weatherDataResult = weatherDataService.GetWeatherDataFromApi(weatherDataViewModel);
+
+            return View("Index", weatherDataResult);
+        }
+    }
+}
